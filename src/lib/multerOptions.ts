2956b63b7952cc585d27eebe46c50567ccc2ed12
황복실 <hwangbogsil@bok.local>
2021-08-18 17:02:02 +0900

    Add: 파일 저장 위치 지정 기능 구현

--- src/lib/multerOptions.ts
+++ src/lib/multerOptions.ts
@@ -1,11 +1,23 @@
-import { UnsupportedMediaTypeException } from "@nestjs/common";
+import { exists, existsSync, mkdirSync } from "fs";
+import { diskStorage } from "multer";
 
 export const multerOptions = {
     fileFilter: (request, file, cb) => {
         if (file.mimetype.match('image/jpeg')) {
-            return cb(null, true);
+            cb(null, true);
         } else {
-            return cb(new Error('지원하지 않는 이미지 형식입니다.'));
+            cb(new Error('지원하지 않는 이미지 형식입니다.'));
         }
-    }
+    },
+    
+    storage: diskStorage({
+        destination: (request, file, cb) => {
+            const uploadPath: string = 'public';
+
+            if (!existsSync(uploadPath)) {
+                mkdirSync(uploadPath);
+            }
+            cb(null, uploadPath);
+        }
+    })
 }
